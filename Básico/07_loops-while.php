2db77e633d8175e9eafac0dbdf1a060df048bec7
Aldara41319 <aldaragarridofdez@gmail.com>
2025-04-05 11:37:40 +0200

    cambios 5/4/2025 - 11:37

--- Básico/07_loops-while.php
+++ Básico/07_loops-while.php
@@ -1,17 +1,36 @@
 <?php
     // Bucle básico while
-    $counter = 0;
+    // $counter = 0;
 
-    while ($counter < 10) {
-        $counter += 1;
-        // echo "Executing - counter is $counter".'<br>';
-    }
+    // while ($counter < 10) {
+    //     $counter += 1;
+    //     echo "Executing - counter is $counter".'<br>';
+    // }
 
     // Hacemos lo mismo pero diciendo cuales son pares
+    // $counter = 0;
+
+    // while ($counter < 10) {
+    //     $counter += 1;
+
+    //     if ($counter % 2 == 0) {
+    //         echo "Skipping number $counter because it is even".'<br>';
+    //         continue;
+    //     }
+
+    //     echo "Executing - counter is $counter".'<br>';
+    // }
+
+    // Haremos como el anterior pero al llegar a 8 para el bucle.
     $counter = 0;
 
     while ($counter < 10) {
         $counter += 1;
+
+        if ($counter > 8) {
+            echo "counter is larger than 8, stopping the loop".'<br>';
+            break;
+        }
 
         if ($counter % 2 == 0) {
             echo "Skipping number $counter because it is even".'<br>';
@@ -20,25 +39,5 @@
 
         echo "Executing - counter is $counter".'<br>';
     }
-
-    // 
-    $counter = 0;
-
-    while ($counter < 10) {
-        $counter += 1;
-
-        if ($counter > 8) {
-            echo "counter is larger than 8, stopping the loop.\n";
-            break;
-        }
-
-        if ($counter % 2 == 0) {
-            echo "Skipping number $counter because it is even.\n";
-            continue;
-        }
-
-        echo "Executing - counter is $counter.\n";
-    }
-
 
 ?>
